1960dd71ad6e1ea0f7810f1e621eeb2351101b9d
Marissa Douglass <mahdouglass@gmail.com>
2019-02-19 21:57:09 -0500

    Set up each state of color for angular animation. I don't think this will be right...

--- src/app/color-animation-demo/color-animation-demo.component.ts
+++ src/app/color-animation-demo/color-animation-demo.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-color-animation-demo',
   templateUrl: './color-animation-demo.component.html',
-  styleUrls: ['./color-animation-demo.component.css']
+  styleUrls: ['./color-animation-demo.component.css'],
+  animations: [
+    trigger('rainbowColorChange', [
+      state('0', style ({
+        backgroundColor: 'red'
+      })),
+      state('20', style ({
+        backgroundColor: 'orange'
+      })),
+      state('40', style ({
+        backgroundColor: 'yellow'
+      })),
+      state('60', style ({
+        backgroundColor: 'green'
+      })),
+      state('80', style ({
+        backgroundColor: 'blue'
+      })),
+      state('100', style ({
+        backgroundColor: 'purple'
+      }))
+    ]),
+  ]
 })
-export class ColorAnimationDemoComponent implements OnInit {
+export class ColorAnimationDemoComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
