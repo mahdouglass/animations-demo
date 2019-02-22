1c49c0034511ab1a0cb3ec49b51a4e5b76a99e78
Marissa Douglass <mahdouglass@gmail.com>
2019-02-21 22:09:19 -0500

    Animation for a fade in and when leave to default back to fade out. Not connected to anything yet.

--- src/app/fade-animation/fade-animation.component.ts
+++ src/app/fade-animation/fade-animation.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition, stagger, query } from '@angular/animations';
 
 @Component({
   selector: 'app-fade-animation',
   templateUrl: './fade-animation.component.html',
-  styleUrls: ['./fade-animation.component.css']
+  styleUrls: ['./fade-animation.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      state('in',
+        style({ opacity: 1 })
+      ),
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms')
+      ]),
+      transition(':leave',
+        animate('300ms', style({ opacity: 0 })
+      ))
+    ])
+  ]
 })
-export class FadeAnimationComponent implements OnInit {
+export class FadeAnimationComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
